7adfe53bfc3bf9c3ad21f25f14af9ce021bb27bd
Anna Polišenská <an.polisenska@gmail.com>
2019-03-15 15:36:20 +0100

    vypoctena sila na srouby -> 325 kN

--- Main/Main/Main.py
+++ Main/Main/Main.py
@@ -44,6 +44,8 @@ def main():
     objDruhaPriruba.d_3 = objPrvniPriruba.d_3
     objDruhaPriruba.E = numpy.asarray([190000,190000])
     objDruhaPriruba.alfa = numpy.asarray([11.0e-6,11.0e-6])
+    objDruhaPriruba.d_S = 408
+    objDruhaPriruba.e_1 = 8
     objDruhaPriruba.sete()
 
     objSrouby = Sroub(1)
